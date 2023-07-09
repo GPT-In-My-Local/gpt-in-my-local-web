f42c49c0cf2ce10285ea0bd1c595f2963dd485ab
hyungjun KIM <hyungjunkim@hyungjunui-MacBookPro.local>
2023-07-09 17:38:41 +0900

    Modify: change env

--- src/login/google.jsx
+++ src/login/google.jsx
@@ -3,7 +3,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 
 const GoogleTest = () => {
-  const clientId = process.env.REACT_APP_CLIENT_ID;
+  // const clientId = process.env.REACT_APP_CLIENT_ID;
+  const clientId ="1067524630700-26bll4iau9498v787t79u5eufk7rnpth.apps.googleusercontent.com";
   const navigate = useNavigate(); //
   return (
     <>
